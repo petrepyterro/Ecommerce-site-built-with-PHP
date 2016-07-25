ac3ad647e5e1275d5950c15628fb5b80b5104152
petrero <petrero@gmail.com>
2016-07-25 22:44:02 +0300

    Custom helper functions.

--- resources/functions.php
+++ resources/functions.php
@@ -1,7 +1,29 @@
 <?php
 
-if($connection){
-  echo "is connected";
+function redirect($location){
+  header("Location: $location");
 }
 
+function query($sql){
+  global $connection;
+  
+  return mysqli_query($connection, $query);
+}
 
+function confirm($result){
+  global $connection;
+  
+  if(!$result){
+    die("QUERY FAILED. " . mysqli_error($connection));
+  }
+}
+
+function escape_string($string){
+  global $connection;
+  
+  return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result){
+  return mysqli_fetch_array($result);
+}
