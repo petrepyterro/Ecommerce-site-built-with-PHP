e07551932b0ea57cbbf757ef7fc9923c8dd3ae3c
petrero <petrero@gmail.com>
2016-07-27 22:04:04 +0300

    Product button link to single product page.

--- resources/functions.php
+++ resources/functions.php
@@ -39,13 +39,13 @@ function get_products(){
     $product = <<<EOD
     <div class="col-sm-4 col-lg-4 col-md-4">
       <div class="thumbnail">
-        <img src="{$row['product_image']}" alt="">
+        <a href="item.php?id={$row['id']}"><img src="{$row['product_image']}" alt=""></a>
         <div class="caption">
           <h4 class="pull-right">&#36;{$row['product_price']}</h4>
           <h4><a href="product.html">{$row['product_title']}</a>
           </h4>
           <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-          <a class="btn btn-primary" target="_blank" href="item.php">Add to cart</a>
+          <a class="btn btn-primary" target="_blank" href="item.php?id={$row['id']}">Add to cart</a>
         </div>
       </div>
     </div>
