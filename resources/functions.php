bc9895df3a9b5d4b830f5d1b1015e778345cfe68
petrero <petrero@gmail.com>
2016-07-27 21:58:37 +0300

    Complete display products on our front-end main page.

--- resources/functions.php
+++ resources/functions.php
@@ -39,13 +39,13 @@ function get_products(){
     $product = <<<EOD
     <div class="col-sm-4 col-lg-4 col-md-4">
       <div class="thumbnail">
-        <img src="http://placehold.it/320x150" alt="">
+        <img src="{$row['product_image']}" alt="">
         <div class="caption">
-          <h4 class="pull-right">\${$row['product_price']}</h4>
-          <h4><a href="product.html">First Product</a>
+          <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+          <h4><a href="product.html">{$row['product_title']}</a>
           </h4>
           <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-          <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+          <a class="btn btn-primary" target="_blank" href="item.php">Add to cart</a>
         </div>
       </div>
     </div>
