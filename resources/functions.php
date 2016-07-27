5809c1f78834fe096e0dee60cfa29f924d581e96
petrero <petrero@gmail.com>
2016-07-27 22:19:16 +0300

    Create a function that list categories on our front-end website pages.

--- resources/functions.php
+++ resources/functions.php
@@ -53,3 +53,15 @@ EOD;
     echo $product;
   }
 }
+
+function get_categories(){
+  $query = query("SELECT * FROM categories");
+  confirm($query);
+
+  while($row = fetch_array($query)){
+    $categories_links = <<<DELIMETER
+      <a href='category.php?id={$row['id']}' class='list-group-item'>{$row['cat_title']}</a>
+DELIMETER;
+    echo $categories_links;  
+  }
+}
