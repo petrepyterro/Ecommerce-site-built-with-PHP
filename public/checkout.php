4a2b445887f32093180f9343c34dc977f430bb16
petrero <petrero@gmail.com>
2016-08-03 22:58:12 +0300

    Thank you page and put the information received from Paypal in our database. Hide the buy now button when no item is in cart.

--- public/checkout.php
+++ public/checkout.php
@@ -12,6 +12,7 @@
       <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
         <input type="hidden" name="cmd" value="_cart">
         <input type="hidden" name="business" value="petrero-facilitator@gmail.com">
+        <input type="hidden" name="currency_code" value="US">
         <table class="table table-striped">
           <thead>
             <tr>
@@ -25,8 +26,7 @@
             <?php cart(); ?>
           </tbody>
         </table>
-        <input type="image" name="upload" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
-          alt="PayPal - The safer, easier way to pay online">
+        <?php echo show_paypal(); ?>
       </form>
 
       <!--  ***********CART TOTALS*************-->
