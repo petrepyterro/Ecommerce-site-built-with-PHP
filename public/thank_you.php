1c28765ea4b91ac33af9d58ebad4faf2502792e4
petrero <petrero@gmail.com>
2016-08-05 23:28:44 +0300

    Copy code from previous branch.

---
+++ public/thank_you.php
@@ -0,0 +1,13 @@
+<?php require_once '../resources/config.php';?>
+<?php include TEMPLATE_FRONT . DS . 'header.php';?>
+<?php 
+  
+  report();
+?>
+  <!-- Page Content -->
+  <div class="container">
+    <h1 class="text-center">Thank You</h1>
+  </div>
+  <!-- /.container -->
+
+  <?php include TEMPLATE_FRONT . DS . 'footer.php';?>
