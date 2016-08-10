80a4dce4c532fc0415217e1d2eda1f3beaa923a5
petrero <petrero@gmail.com>
2016-08-10 20:25:04 +0300

    Copy code from previous branch.

---
+++ resources/templates/back/delete_order.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../../config.php';
+
+if (isset($_GET['id'])){
+  $query = query("DELETE FROM orders WHERE id=" . escape_string($_GET['id']));
+  confirm($query);
+  
+  set_message("Order Deleted");
+  redirect("../../../public/admin/index.php?orders");
+} else {
+  redirect("../../../public/admin/index.php?orders");
+}
+
