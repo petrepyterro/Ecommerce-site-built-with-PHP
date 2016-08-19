6d541dc287c3e56f099804cbb6a4280c80d375f9
petrero <petrero@gmail.com>
2016-08-19 21:24:17 +0300

    Fixing templates paths.

--- public/resources/templates/back/delete_order.php
+++ public/resources/templates/back/delete_order.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once '../../config.php';
+require_once '../../../../resources/config.php';
 
 if (isset($_GET['id'])){
   $query = query("DELETE FROM orders WHERE id=" . escape_string($_GET['id']));
   confirm($query);
   
   set_message("Order Deleted");
-  redirect("../../../public/admin/index.php?orders");
+  redirect("../../../admin/index.php?orders");
 } else {
-  redirect("../../../public/admin/index.php?orders");
+  redirect("../../../admin/index.php?orders");
 }
 
--- public/resources/templates/back/delete_user.php
+++ public/resources/templates/back/delete_user.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once '../../config.php';
+require_once '../../../../resources/config.php';
 
 if (isset($_GET['id'])){
   $query = query("DELETE FROM users WHERE id=" . escape_string($_GET['id']));
   confirm($query);
   
   set_message("User Deleted");
-  redirect("../../../public/admin/index.php?users");
+  redirect("../../../admin/index.php?users");
 } else {
-  redirect("../../../public/admin/index.php?users");
+  redirect("../../../admin/index.php?users");
 }
 
