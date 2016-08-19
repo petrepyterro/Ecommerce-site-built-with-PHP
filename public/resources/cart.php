bb53c56bf9b4982002c347b6f3d378b72b69d19f
petrero <petrero@gmail.com>
2016-08-19 20:18:44 +0300

    Fixing paths in cart function used by checkout.php file

--- public/resources/cart.php
+++ public/resources/cart.php
@@ -62,15 +62,15 @@ function cart(){
           <tr>
             <td>
               {$row['product_title']}<br>
-              <img width='100' src="../resources/{$product_image}"/>
+              <img width='100' src="resources/{$product_image}"/>
             </td>
             <td>&#36;{$row['product_price']}</td>
             <td>{$value}</td>
             <td>{$sub}</td>
             <td>
-              <a class='btn btn-warning' href="../resources/cart.php?remove={$row['id']}"><span class='glyphicon glyphicon-minus'></span></a>
-              <a class='btn btn-success' href="../resources/cart.php?add={$row['id']}"><span class='glyphicon glyphicon-plus'></span></a>
-              <a class='btn btn-danger' href="../resources/cart.php?delete={$row['id']}"><span class='glyphicon glyphicon-remove'></span></a>
+              <a class='btn btn-warning' href="resources/cart.php?remove={$row['id']}"><span class='glyphicon glyphicon-minus'></span></a>
+              <a class='btn btn-success' href="resources/cart.php?add={$row['id']}"><span class='glyphicon glyphicon-plus'></span></a>
+              <a class='btn btn-danger' href="resources/cart.php?delete={$row['id']}"><span class='glyphicon glyphicon-remove'></span></a>
             </td> 
           </tr>
           <input type="hidden" name="item_name_{$item_name}" value="{$row['product_title']}">
