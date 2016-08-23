1b1cb9940eaea2dc67b1cf786440934ea6c21205
petrero <petrero@gmail.com>
2016-08-23 21:35:35 +0300

    Delete the record of the image slide in database and the corespondent file from upload server directory in the same time.

--- resources/templates/back/delete_slide.php
+++ resources/templates/back/delete_slide.php
@@ -3,8 +3,16 @@
 require_once '../../resources/config.php';
 
 if (isset($_GET['delete_slide_id'])){
+  $image_name_query = query("SELECT slide_image FROM slides WHERE id=" . escape_string($_GET['delete_slide_id']) . " LIMIT 1");
+  confirm($image_name_query);
+  $row = fetch_array($image_name_query);
+  $target_path = UPLOAD_DIRECTORY . DS . $row['slide_image'];
+  unlink($target_path);
+  
+  
   $query = query("DELETE FROM slides WHERE id=" . escape_string($_GET['delete_slide_id']));
   confirm($query);
+  
   
   set_message("Slide Deleted");
   redirect("../../../public/admin/index.php?slides");
