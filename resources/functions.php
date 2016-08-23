dc3fb8027a8d98ccc4c71f7fb356e206b2945023
petrero <petrero@gmail.com>
2016-08-23 20:57:43 +0300

    Apply some styles to thumbnails slides displayed on add slides page.

--- resources/functions.php
+++ resources/functions.php
@@ -565,7 +565,7 @@ function get_slide_thumbnails(){
     $slide_thumb_admin = <<<SLIDES
       <div class="col-xs-6 col-md-3">
         <a href="">
-          <img class="img-responsive" src="../../resources/{$slide_image}" alt="">
+          <img class="img-responsive slide_image" src="../../resources/{$slide_image}" alt="">
         </a>
       </div>       
 SLIDES;
